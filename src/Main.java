e97c83889f7d2e1004e4e026be50cd93282d6480
Afrim Ymeri <78933251+afrimymeri@users.noreply.github.com>
2025-02-15 13:44:09 +0100

    Generate Parentheses, Problem 22.

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 public class Main{
     public static void main(String [] args) {
+        System.out.println(Solutions.generateParenthesis(4));
     }
 
     public static ArrayList<String> fizzbuzz(int nums[]) {
@@ -121,6 +122,24 @@ class Solutions {
         }else{
             list2.next = mergeTwoLinkedLists(list1, list2.next);
             return list2;
+        }
+    }
+
+    public static List<String> generateParenthesis(int n) {
+        List<String> result = new ArrayList<>();
+        backtrack(result, "", 0,0,n);
+        return result;
+    }
+    private static void backtrack(List<String> result, String current, int open, int close, int max) {
+        if (current.length() == max *2) {
+            result.add(current);
+            return;
+        }
+        if (open < max) {
+            backtrack(result, current + "(", open +1, close, max);
+        }
+        if (max> open) {
+            backtrack(result, current + ")",open,  close+ 1, max);
         }
     }
 
